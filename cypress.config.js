bbb59097e91649ae2ee94c1ddc6a3a0a8c64f0fc
firdos9 <wasilfirdos@gmail.com>
2025-09-08 18:44:07 +0300

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   e2e: {
       specPattern: 'cypress/e2e/**/*.js', // tells Cypress where to find spec files
       setupNodeEvents(on, config) {
-    // you can leave this as-is
+    
     },
   },
 });

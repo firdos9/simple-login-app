eca48acf47dc51515b821fc9abb40e751aa46f56
Eskedar Tafa <Selamtafa1@gmail.com>
2025-09-14 01:35:07 +0300

    chore(config): configure Cypress for project testing

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,10 @@
+ 
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-      specPattern: 'cypress/e2e/**/*.js', // tells Cypress where to find spec files
+      specPattern: 'cypress/e2e/**/*.js',
+  // tells Cypress where to find spec files
       setupNodeEvents(on, config) {
     
     },

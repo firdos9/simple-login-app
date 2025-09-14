96f4cb2633ee52d580139882be517656edcdb9d0
Eskedar Tafa <Selamtafa1@gmail.com>
2025-09-14 13:45:26 +0300

    : configure Cypress for project testing

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,17 @@
- 
+//cypress.config.js
+
+//ensure cypress loads the apps frome running Express server
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
       specPattern: 'cypress/e2e/**/*.js',
   // tells Cypress where to find spec files
+   
+   
       setupNodeEvents(on, config) {
     
     },
   },
 });
+
